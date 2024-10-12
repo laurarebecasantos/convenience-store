106df0cc9b9f695e9676bb913e8b49ddb41cb920
Laura <laurarebecasantos@gmail.com>
2024-10-12 02:21:57 -0300

    feat: create TokenGenerationException class for JWT errors

---
+++ src/main/java/com/api/rest/conveniencestore/exceptions/TokenGenerationException.java
@@ -0,0 +1,7 @@
+package com.api.rest.conveniencestore.exceptions;
+
+public class TokenGenerationException extends Exception{
+    public TokenGenerationException(String message) {
+        super(message);
+    }
+}
